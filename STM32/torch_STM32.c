5ddb2394fb74ac8b1886b0ad5257f1281f8d7b7a
shayan-ehtezazi <sehtezazi01@gmail.com>
2025-03-21 13:36:11 -0400

    HALing the STM32 HAL

--- STM32/torch_STM32.c
+++ STM32/torch_STM32.c
@@ -1,12 +1,20 @@
 #include "main.h"
 #include "torch_STM32.h"
 
-void pull_low(GPIO_TypeDef *port_ptr, uint16_t pin) { HAL_GPIO_WritePin(port, pin, GPIO_PIN_RESET); }
+volatile uint32_t Counter = 0;
 
-void pull_high(GPIO_TypeDef *port_ptr, uint16_t pin) { HAL_GPIO_WritePin(port, pin, GPIO_PIN_SET); }
+void pull_low(GPIO_TypeDef *port_ptr, uint16_t pin)
+{
+	HAL_GPIO_WritePin(port, pin, GPIO_PIN_RESET);
+}
+
+void pull_high(GPIO_TypeDef *port_ptr, uint16_t pin)
+{
+	HAL_GPIO_WritePin(port, pin, GPIO_PIN_SET);
+}
 
 // Transmits SPI messages on the specified bus
-uint8_t SPI_transmit(SPI_HandleTypeDef *hspi, uint8_t *data_ptr, uint8_t dataLen)
+uint8_t SPI_transmit(SPI_HandleTypeDef *hspi, uint8_t *transmissionData_ptr, uint8_t transmissionDataLen)
 {
 	HAL_StatusTypeDef status;		// status indicates whether the transmission was successful or not
 	uint8_t attempts = 0;			// transmission attempts (set to 3)
@@ -15,7 +23,7 @@ uint8_t SPI_transmit(SPI_HandleTypeDef *hspi, uint8_t *data_ptr, uint8_t dataLen
 		// 1 -> Error
 		
 	while(attempts < 3) {
-		status = HAL_SPI_Transmit(hspi, data_ptr, dataLen, HAL_MAX_DELAY);
+		status = HAL_SPI_Transmit(hspi, transmissionData_ptr, transmissionDataLen, HAL_MAX_DELAY);
 		
 		if(status == HAL_OK) { attempts = 10; }
 		
@@ -25,6 +33,85 @@ uint8_t SPI_transmit(SPI_HandleTypeDef *hspi, uint8_t *data_ptr, uint8_t dataLen
 		}
 	}
 	if(attempts == 10) { return 0; }
+	
+	else { return 1; }
+}
+
+// Receives SPI messages on specified bus
+uint8_t SPI_receive(SPI_HandleTypeDef *hspi, uint8_t *receptionData_ptr, uint8_t receptionDataLen)
+{
+	HAL_StatusTypeDef status;													// status indicates whether the reception was successful or not
+	uint8_t attempts = 0;														// reception attempts (set to 3)
+	uint8_t dummies[8] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};		// dummy bytes to send
+	// Here are the possible values that the function can return:
+		// 0 -> Message successfully received
+		// 1 -> Error
+	
+	while(attempts < 3) {
+		status = HAL_SPI_TransmitReceive(hspi, dummies, receptionData_ptr, receptionDataLen, HAL_MAX_DELAY);
+		
+		if(status == HAL_OK) { attempts = 10; }
+		
+		else {
+			attempts++;
+			HAL_Delay(1);
+		}
+	}
+	if(attempts == 10) { return 0; }
+	
+	else { return 1; }
+}
+
+
+uint8_t start_timer(TIM_HandleTypeDef *htim)
+{
+	HAL_StatusTypeDef status;										// status indicates whether the reception was successful or not
+	uint8_t attempts = 0;											// 3 attempts
+	// Here are the possible values that the function can return:
+		// 0 -> Timer started successfully
+		// 1 -> Error
+	
+	Counter = 0;
+	
+	while(attempts < 3) {
+		status = HAL_TIM_Base_Start_IT(htim);
+		
+		if(status == HAL_OK) { attempts = 10; }
+		
+		else {
+			attempts++;
+			HAL_Delay(1);
+		}
+	}
+	if(attempts == 10) { return 0; }
+	
+	else { return 1; }
+}
+
+
+uint8_t stop_timer(TIM_HandleTypeDef *htim)
+{
+	HAL_StatusTypeDef status;										// status indicates whether the reception was successful or not
+	uint8_t attempts = 0;											// 3 attempts
+	// Here are the possible values that the function can return:
+		// 0 -> Timer stopped successfully
+		// 1 -> Error
+	
+	while(attempts < 3) {
+		status = HAL_TIM_Base_Stop_IT(htim);
+		
+		if(status == HAL_OK) { attempts = 10; }
+		
+		else {
+			attempts++;
+			HAL_Delay(1);
+		}
+	}
+	if(attempts == 10) {
+		__HAL_TIM_SET_COUNTER(htim, 0);
+		Counter = 0;
+		return 0;
+	}
 	
 	else { return 1; }
 }
